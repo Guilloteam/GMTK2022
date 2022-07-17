7c443fc12b2b29ae85012c3c0723c0a0b085de90
Julien Revel <j.revel@dowino.com>
2022-07-17 17:58:55 +0200

    Added animations to shooting enemy

--- Project/Assets/Scripts/Gameplay/Enemies/ShootingEnemyAI.cs
+++ Project/Assets/Scripts/Gameplay/Enemies/ShootingEnemyAI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShootingEnemyAI : MonoBehaviour
 {
+    public MeshRenderer meshRenderer;
     private MovementController movementController;
     public float movementSpeed = 1;
     public Transform target;
@@ -14,6 +15,12 @@ public class ShootingEnemyAI : MonoBehaviour
 
     public Rigidbody projectilePrefab;
     public float projectileSpeed = 1;
+    public Transform squashAnimTarget;
+    public float maxSquash = 0.7f;
+    public Material defaultMaterial;
+    public Material shootMaterial;
+    public float shootMaterialAppearDuration = 0.5f;
+    private float shootMaterialAppearTime = 0;
     
     void Start()
     {
@@ -25,13 +32,22 @@ public class ShootingEnemyAI : MonoBehaviour
     {
         Vector3 direction = (target.position - transform.position);
         direction.y = 0;
+        if(shootMaterialAppearTime > 0)
+        {
+            shootMaterialAppearTime -= Time.deltaTime;
+            meshRenderer.sharedMaterial = shootMaterial;
+            if(shootMaterialAppearTime < 0)
+                meshRenderer.sharedMaterial = defaultMaterial;
+        }
         if(direction.sqrMagnitude < shootingMinRange * shootingMinRange)
         {
+            squashAnimTarget.localScale = Vector3.one;
             movementController.inputDirection = -direction.normalized;
             loadingTime = 0;
         }
         else if(direction.sqrMagnitude > shootingMaxRange * shootingMaxRange)
         {
+            squashAnimTarget.localScale = Vector3.one;
             movementController.inputDirection = direction.normalized;
             loadingTime = 0;
         }
@@ -39,10 +55,13 @@ public class ShootingEnemyAI : MonoBehaviour
         {
             movementController.inputDirection = Vector3.zero;
             loadingTime += Time.deltaTime;
+            squashAnimTarget.localScale = new Vector3(1, 1, Mathf.Lerp(1, maxSquash, loadingTime / loadingDuration));
             if(loadingTime > loadingDuration)
             {
+                squashAnimTarget.localScale = Vector3.one;
                 Instantiate(projectilePrefab, transform.position, projectilePrefab.rotation).velocity = direction.normalized * projectileSpeed;
                 loadingTime = 0;
+                shootMaterialAppearTime = shootMaterialAppearDuration;
             }
         }
     }
