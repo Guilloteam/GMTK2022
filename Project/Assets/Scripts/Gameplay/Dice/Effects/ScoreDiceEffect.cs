4315ef76b36bf10dd84bab8580f9f24a2d4afeb8
Julien Revel <j.revel@dowino.com>
2022-07-17 15:14:24 +0200

    Score handling + turn player into prefab

---
+++ Project/Assets/Scripts/Gameplay/Dice/Effects/ScoreDiceEffect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDiceEffect : MonoBehaviour
+{
+    public int score;
+    public ScoreBonus bonusPrefab; 
+
+    void Start()
+    {
+        float multiplier = ScoreMultiplierEffect.GetCurrentMultiplier();
+        ScoreSystem.instance.score += score * multiplier;
+        Instantiate(bonusPrefab, transform.position, bonusPrefab.transform.rotation).bonus = (int)(score * multiplier);
+    }
+}
